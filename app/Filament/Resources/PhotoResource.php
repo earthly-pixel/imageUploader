347f54acf926deb684c5f146b066ee382146b23f
Angga Septiaji <angga@debian12.magelangkota.go.id>
2025-08-04 10:01:21 +0700

    add download button

--- app/Filament/Resources/PhotoResource.php
+++ app/Filament/Resources/PhotoResource.php
@@ -86,7 +86,8 @@ class PhotoResource extends Resource
                 // Tables\Actions\EditAction::make(),
                 Tables\Actions\Action::make('download')
                     ->action(function($record) {
-                        return Storage::download($record->file, $record->name);
+                        // dd(Storage::disk('public')->path($record->file));
+                        return Storage::disk('public')->download($record->file, $record->name);
                     })
                     ->color('primary'),
             ])
